68fb63c9f69c4054916b4653dd23804e887e9d45
ISSQ1 <shosho2626@hotmail.com>
2024-08-20 08:40:55 +0300

    My solutions

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,10 @@
 def players_by_position(squads_list):
-    pass
+    result={}
+    from football_dictionaries.assignment_1 import players_as_dictionaries
+    players= players_as_dictionaries(squads_list)
+    for player in players:
+        position = player['position']
+        if position not in result:
+            result[position] = []
+        result[position].append(player)
+    return result
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,13 @@
 def players_by_country_and_position(squads_list):
-    pass
+    result={}
+    from football_dictionaries.assignment_1 import players_as_dictionaries
+    players= players_as_dictionaries(squads_list)
+    for player in players:
+        country = player['country']
+        position = player['position']
+        if country not in result:
+            result[country]={}
+        if position not in result[country]:
+            result[country][position]=[]
+        result[country][position].append(player)
+    return result
